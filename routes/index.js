2f45651fb9cf65791c662aa48871241156a25613
Shivam Shukla <shivam@gkmit.co>
2024-04-09 14:27:35 +0530

    create login api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { registerUser, loginUser } from '../userController/index.js';
-import {userRegisterValidation} from "../utils/userValidation.js"
+import {
+	userLoginValidate,
+	userRegisterValidation,
+} from '../utils/userValidation.js';
 const routes = express.Router();
 
 // Routes
 routes.post('/register', userRegisterValidation, registerUser);
-routes.post('/login', loginUser);
+routes.post('/login', userLoginValidate, loginUser);
 
 export default routes;
